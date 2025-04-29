bcf4025f48635c45a54850885b5c1d6885f6f8b2
yassin toumeh <yassintoumeh20@gmail.com>
2025-04-30 02:11:28 +0300

    initial header structure

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <nav>
+            <div class="logo-container"></div>
+            <div class="navigation-link"></div>
+            <div class="header-action">
+                <div class="additional-link"></div>
+                <div class="bottom-container"></div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
