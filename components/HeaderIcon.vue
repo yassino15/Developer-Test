379060ece7c548b8b91b5bd1c720e2ef5c35aaba
yassin toumeh <yassintoumeh20@gmail.com>
2025-04-30 16:31:30 +0300

    header icon component

---
+++ components/HeaderIcon.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="d-flex" :class="containerClass">
+        <NuxtLink to="/login" class="mx-2" aria-label="Account">
+            <NuxtImg src="/icons/login.png" alt="login" width="25" />
+        </NuxtLink>
+        <NuxtLink to="/wish-list" class="mx-2" aria-label="Wishlist">
+            <NuxtImg src="/icons/heart.png" alt="fav" width="25" />
+        </NuxtLink>
+        <NuxtLink to="/cart" class="ms-2" aria-label="Cart">
+            <NuxtImg src="/icons/Cart.png" alt="cart" width="25" />
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    containerClass: {
+        type: String,
+        default: ''
+    },
+});
+</script>
