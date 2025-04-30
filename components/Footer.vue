51f6b69afafc089558131f082948362dbdd70c26
yassin toumeh <yassintoumeh20@gmail.com>
2025-04-30 16:39:59 +0300

    initial footer structure

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container-fluid">
+        <div class="row px-5">
+            <div class="col-4"></div>
+            <div class="col-2"></div>
+            <div class="col-2"></div>
+            <div class="col-4"></div>
+        </div>
+        <hr>
+        <p class="text-center">
+            &copy; Developer Test 2024. All Right Reserved.
+        </p>
+    </div>
+</template>
+<script setup></script>
+<style scoped></style>
