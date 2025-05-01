c9a8b5b7ac69e17b4a63ffdad2c2b0325f5eaf56
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-01 12:12:55 +0300

    gather commod links

---
+++ components/MainNavLinks.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :class="containerClass">
+        <NuxtLink v-for="link in links" :key="link.path" :to="link.path" :class="linkClass">
+            {{ link.title }}
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    containerClass: {
+        type: String,
+        default: ''
+    },
+    linkClass: {
+        type: String,
+        default: ''
+    }
+})
+const links = [
+    { title: 'Electronics', path: '/electronics' },
+    { title: 'Jewelry', path: '/jewelry' },
+    { title: 'Men Clothing', path: '/men-clothing' },
+    { title: 'Women Clothing', path: '/women-clothing' }
+];
+</script>
---
+++ components/SecondaryNavLinks.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="containerClass">
+        <NuxtLink v-for="link in links" :key="link.path" :to="link.path" :class="linkClass">
+            {{ link.title }}
+        </NuxtLink>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    containerClass: {
+        type: String,
+        default: ''
+    },
+    linkClass: {
+        type: String,
+        default: 'nav-link mx-2'
+    }
+});
+const links = [
+    { title: 'Home', path: '/' },
+    { title: 'About Us', path: '/about-us' },
+    { title: 'Media Center', path: '/media-center' },
+    { title: 'Contact Us', path: '/contact-us' }
+];
+</script>
