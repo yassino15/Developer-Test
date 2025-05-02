317c1af96e3821b518d3892cdc6b9dceaeb8ad24
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-02 14:59:29 +0300

    setup hero section

--- components/HeroSaleSection.vue
+++ components/HeroSaleSection.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
+    <div class="hero-sale-container">
+        <Swiper :modules="[Pagination, Autoplay]" :autoplay="{ delay: 20000 }" :pagination="{
+            clickable: true,
+            el: '.swiper-custom-pagination'
+        }" class="my-swiper">
+            <SwiperSlide v-for="(slide, i) in slides" :key="i">
+                <div class="hero-slide" :style="{ backgroundImage: `url(${slide.image})` }"></div>
+            </SwiperSlide>
+        </Swiper>
 
+        <div class="swiper-custom-pagination"></div>
     </div>
 </template>
 
 <script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination, Autoplay } from 'swiper/modules'
 
+const slides = ref([
+    { image: '/Images/Header.jpg', title: 'Slide 1 Title', buttonImg: '/icons/button.png' },
+    { image: '/Images/Header.jpg', title: 'Slide 2 Title', buttonImg: '/icons/button.png' },
+])
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+@import url('@/assets/css/hero.css');
 </style>
