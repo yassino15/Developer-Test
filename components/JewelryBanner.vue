aeaeb77af58737cf72deb4c7fc569cce3c194e65
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-02 22:17:35 +0300

    implement jewelry banner

--- components/JewelryBanner.vue
+++ components/JewelryBanner.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-
+    <div class="position-relative">
+        <NuxtImg src="/Images/Jewelry.jpg" class="w-100" alt="Jewelry" format="webp" />
+        <div class="jwelry-banner-button">
+            <CustomButton bgColor="rgb(238 238 228)" color="#000000" text="see more" />
+        </div>
     </div>
 </template>
 
 <script setup>
-
 </script>
-
-<style lang="scss" scoped>
-
+<style>
+/* @import url('@/assets/css/jewelry.css'); */
 </style>
