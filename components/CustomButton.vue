6438d01d14c0ea195a4ab393e971411e1445f7e3
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-02 22:49:14 +0300

    improve button responsivity

--- components/CustomButton.vue
+++ components/CustomButton.vue
@@ -6,7 +6,7 @@
             <span class="arrow-icon" :class="{ 'arrow-hovered': isHovered }">
                 <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" :fill="color"
                     text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd"
-                    clip-rule="evenodd" viewBox="0 0 512 243.58" width="42">
+                    clip-rule="evenodd" viewBox="0 0 512 243.58" width="35">
                     <path fill-rule="nonzero"
                         d="M373.57 0 512 120.75 371.53 243.58l-20.92-23.91 94.93-83L0 137.09v-31.75l445.55-.41-92.89-81.02z" />
                 </svg>
@@ -98,17 +98,17 @@ const isHovered = ref(false);
 
 @media screen and (max-width: 768px) {
     .custom-button {
-        padding: 4px 6px;
+        padding: 5px 9px;
         width: 125px;
         letter-spacing: .2px;
     }
 
     .button-text {
-        font-size: .8rem;
+        font-size: .6rem;
     }
 
     .arrow-icon {
-        margin: 0;
+        margin: 8px;
     }
 }
 </style>
