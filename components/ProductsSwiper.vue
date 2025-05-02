2d1557db6223db791d61b58106d32d851c4ff2e1
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-02 22:13:07 +0300

    create product group swiper

---
+++ components/ProductsSwiper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="hero-sale-container">
+        <Swiper :modules="[Pagination, Autoplay]" :pagination="{
+            clickable: true,
+            el: '.swiper-custom-pagination'
+        }" :breakpoints="{
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 15
+            },
+            1024: {
+                slidesPerView: slidesPerView || 4,
+                spaceBetween: 20
+            }
+        }" class="my-swiper">
+            <SwiperSlide v-for="(product, i) in products" :key="i">
+                <ProductCard :product="product">
+                    <slot name="product-info" :product="product"></slot>
+                </ProductCard>
+            </SwiperSlide>
+            <div class="swiper-custom-pagination"></div>
+        </Swiper>
+    </div>
+</template>
+
+
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination, Autoplay } from 'swiper/modules'
+defineProps({
+    products: {
+        type: Array,
+        required: true
+    },
+    slidesPerView: Number,
+    default: 4
+})
+</script>
+
+<style scoped>
+@import url('@/assets/css/hero.css');
+</style>
