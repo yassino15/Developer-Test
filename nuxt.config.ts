136f82cfebc63abefe4f311862e291e3bbee238f
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-02 23:33:12 +0300

    install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', 'nuxt-swiper'],
+  modules: ['@nuxt/image', 'nuxt-swiper', '@pinia/nuxt'],
   css: [
     '~/assets/css/main.css',
     'swiper/css',
