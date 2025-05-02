fe8abd92af4d6d36ee6f21941c71b582b051330e
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-02 22:48:37 +0300

    implement promo

--- components/MembershipPromo.vue
+++ components/MembershipPromo.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-
+    <div
+        class="promo-container container-fluid text-uppercase d-flex flex-column flex-md-row justify-content-center align-items-center gap-4 gap-md-5">
+        <p class="promo-text fs-1 font-dubai-bold m-0 text-center text-md-start">
+            become a member & get 15% off
+        </p>
+        <CustomButton text="SIGN UP FOR FREE" bgColor="black" color="white" />
     </div>
 </template>
 
@@ -8,4 +12,6 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+@import url('@/assets/css/promo.css');
+</style>
