b56bc362b3ea3fd97361ff0459e89470345daf8e
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-03 10:01:19 +0300

    create sort prod by category

---
+++ components/SortDropdown.vue
@@ -0,0 +1,20 @@
+<template>
+    <select class="form-select" v-model="modelValue" @change="onChange">
+        <option value="default">Sort by</option>
+        <option value="price-asc">Price: Low to High</option>
+        <option value="price-desc">Price: High to Low</option>
+        <option value="rating-desc">Highest Rated</option>
+    </select>
+</template>
+
+<script setup>
+defineProps({
+    modelValue: String
+})
+
+defineEmits(['update:modelValue'])
+
+function onChange() {
+    emit('update:modelValue', modelValue)
+}
+</script>
