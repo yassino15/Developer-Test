b64a320cb7afd823ddce7b0650518faf6940c04f
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-03 19:56:56 +0300

    edit loader spacing

--- components/Loader.vue
+++ components/Loader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-100 text-center">
-        <NuxtImg src="/loader.svg" alt="loader" />
+    <div class="w-100 text-center my-5 py-5">
+        <NuxtImg src="/loader.svg" alt="loader" class="my-5 py-4" />
     </div>
 </template>
