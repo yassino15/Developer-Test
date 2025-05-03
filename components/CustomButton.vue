1640ee32078735a62ba0b3ed3eaa38e6d437aba0
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-03 11:50:46 +0300

    add arrow as component

--- components/CustomButton.vue
+++ components/CustomButton.vue
@@ -4,12 +4,7 @@
             @mouseleave="isHovered = false" @click="$emit('click')">
             <span class="button-text font-dubai-medium" :class="{ 'text-hovered': isHovered }">{{ text }}</span>
             <span class="arrow-icon" :class="{ 'arrow-hovered': isHovered }">
-                <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" :fill="color"
-                    text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd"
-                    clip-rule="evenodd" viewBox="0 0 512 243.58" width="35">
-                    <path fill-rule="nonzero"
-                        d="M373.57 0 512 120.75 371.53 243.58l-20.92-23.91 94.93-83L0 137.09v-31.75l445.55-.41-92.89-81.02z" />
-                </svg>
+                <ArrowIcon :fill="color" />
             </span>
         </button>
         <div class="button-outline" :class="{ 'outline-active': isHovered }" :style="{ borderColor: bgColor }"></div>
