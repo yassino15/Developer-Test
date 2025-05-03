f369ab3135cb022956d0f572e9ae593aa030e5b9
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-03 11:51:08 +0300

    add navigation buttons

--- components/ProductsSwiper.vue
+++ components/ProductsSwiper.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="hero-sale-container">
-        <Swiper :modules="[Pagination, Autoplay]" :pagination="{
+        <Swiper :modules="[Pagination, Autoplay, Navigation]" :pagination="{
             clickable: true,
             el: '.swiper-custom-pagination'
+        }" :navigation="{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
         }" :breakpoints="{
             320: {
                 slidesPerView: 1,
@@ -18,11 +21,17 @@
             }
         }" class="my-swiper">
             <SwiperSlide v-for="(product, i) in products" :key="i">
-                <ProductCard :product="product">
+                <ProductCardSlide :product="product">
                     <slot name="product-info" :product="product"></slot>
-                </ProductCard>
+                </ProductCardSlide>
             </SwiperSlide>
             <div class="swiper-custom-pagination"></div>
+            <div class="swiper-button-prev custom-nav" v-if="showNavButtons">
+                <ArrowIcon :flip="true" />
+            </div>
+            <div class="swiper-button-next custom-nav" v-if="showNavButtons">
+                <ArrowIcon :flip="false" />
+            </div>
         </Swiper>
     </div>
 </template>
@@ -30,17 +39,20 @@
 
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Pagination, Autoplay } from 'swiper/modules'
+import { Pagination, Autoplay, Navigation } from 'swiper/modules'
+
 defineProps({
     products: {
         type: Array,
         required: true
     },
-    slidesPerView: Number,
-    default: 4
+    slidesPerView: {
+        type: Number,
+        default: 4
+    },
+    showNavButtons: {
+        type: Boolean,
+        default: false
+    }
 })
 </script>
-
-<style scoped>
-@import url('@/assets/css/hero.css');
-</style>
