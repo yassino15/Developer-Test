08d573603c90f6cdcf50d300a0c7b31298460146
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-03 09:24:12 +0300

    rename component

---
+++ components/productCardSlide.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card border-0">
+        <NuxtImg class="card-img-top rounded-0" :src="product.image" alt="product image" />
+        <div class="card-body pt-1">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    product: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<style scoped></style>
