e8aeeadce300913191da980a90634adfa6a819f7
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-03 10:01:36 +0300

    product search

---
+++ components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search products..." v-model="modelValue"
+            @input="updateValue" />
+        <button class="btn btn-primary" type="button" @click="onSearch">
+            <i class="bi bi-search"></i> Search
+        </button>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    modelValue: String
+})
+
+defineEmits(['update:modelValue', 'search'])
+
+function updateValue(event) {
+    emit('update:modelValue', event.target.value)
+}
+
+function onSearch() {
+    emit('search')
+}
+</script>
