c1188b683b7ce3080a04a750d0604ac8dcbaca0b
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-03 09:24:42 +0300

    create product bread crumb

---
+++ components/ProductBreadCrumb.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav aria-label="breadcrumb" class="mb-4">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <NuxtLink to="/" class="text-decoration-none">Home</NuxtLink>
+            </li>
+            <li class="breadcrumb-item">
+                <NuxtLink :to="`/products/${product.category}`" class="text-decoration-none">{{ product.category }} products
+                </NuxtLink>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">{{ product?.title || 'Product Detail' }}</li>
+        </ol>
+    </nav>
+</template>
+
+<script setup>
+defineProps({
+    product: {
+        type: Object,
+        required: false
+    }
+})
+</script>
