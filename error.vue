d522f308a0037c1644211056200c4239d056e93a
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-03 11:28:37 +0300

    update error page

--- error.vue
+++ error.vue
@@ -1,4 +1,18 @@
 <template>
-  <div></div>
+  <div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="card shadow p-4" style="max-width: 400px; width: 100%;">
+      <div class="card-body text-center">
+        <h1 class="display-4 text-danger mb-3">Error {{ error.statusCode }}</h1>
+        <p class="lead mb-4">{{ error.statusMessage }}</p>
+        <button @click="clearErr" class="btn btn-primary">Back to home</button>
+      </div>
+    </div>
+  </div>
 </template>
-<script setup></script>
+
+<script setup>
+defineProps(['error']);
+const clearErr = () => {
+  clearError({ redirect: '/' });
+};
+</script>
