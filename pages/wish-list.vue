4782262cd1a2c08bc8d94077d8c85244e77955e3
yassin toumeh <yassintoumeh20@gmail.com>
2025-05-03 23:32:41 +0300

    temp wish list page

--- pages/wish-list.vue
+++ pages/wish-list.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-
+    <div class="py-5 my-5">
+        <div class="bg-light p-4 rounded text-center shadow-sm">
+            <h4 class="mb-3">Favorites Feature</h4>
+            <p class="text-muted mb-0">✨ This feature is coming soon! You’ll be able to save and manage your favorite
+                services for quick access. Stay tuned!</p>
+        </div>
     </div>
 </template>
-
-<script setup>
-
-</script>
